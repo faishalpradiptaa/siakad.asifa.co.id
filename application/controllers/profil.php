2b7b943c668f8fb267914e1809242e21662c703d
Faishal Pradipta <shirage68@gmail.com>
2018-08-08 17:57:41 +0700

    profil

--- application/controllers/profil.php
+++ application/controllers/profil.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class profil extends admin_controller {
@@ -18,24 +17,4 @@ class profil extends admin_controller {
 
 
 }
-=======
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class profil extends admin_controller {
-
-	public $title = 'Profil';
-
-	public function index()
-	{
-		$this->load->model('mod_siswa');
-		$pack = array(
-			'tahun_ajaran' => $this->mod_siswa->getTahunAjaran(),
-			'detail_siswa' => $this->detail_siswa
-		);
-		$this->load->template('profil/v_view', $pack);
-	}
-
-
-
-}
->>>>>>> 2c4a65a97f892b1ddefd99f5917efa022830a485
+?>
